4e8533c654c77d9aebd022e52e19cbd94d8da387
Wilson Huynh <wilsonhuynh@gmail.com>
2026-07-15 17:50:19 +1000

    Set Components display order for Button to be first

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -10,6 +10,11 @@ const preview: Preview = {
       },
     },
     layout: "centered",
+    options: {
+      storySort: {
+        order: ["Components", ["Button", "Typography"]],
+      },
+    },
   },
 };
 
